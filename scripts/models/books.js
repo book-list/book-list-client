40df3419ed25e1dac0671f61ef934357b861d6d6
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-08 12:27:44 -0800

    testing

--- scripts/models/books.js
+++ scripts/models/books.js
@@ -29,11 +29,16 @@ var __API_URL__ = 'https://kc-ng-booklist.herokuapp.com/';
       .then(callback)
       .catch(errorCallback);
 
-  Book.prototype.insertBook = function(callback) {
-    $.post('/books', {author: this.author, title: this.title, isbn: this.isbn, image_url: this.image_url, description: this.description})
-      .then(console.log)
-      .then(callback);
-  };
+  Book.createBook = book =>
+    $.post(`${__API_URL__}/books`, book)
+      .then(() => page('/'))
+      .catch(errorCallback);
+
+  // Book.prototype.insertBook = function(callback) {
+  //   $.post('/books', {author: this.author, title: this.title, isbn: this.isbn, image_url: this.image_url, description: this.description})
+  //     .then(console.log)
+  //     .then(callback);
+  // };
 
   module.Book = Book;
 })(app)
