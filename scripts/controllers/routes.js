a3aed975ce92e0570b2cd63d5e85696f1d774d5f
Kat Cosgrove <kat.cosgrove@gmail.com>
2017-11-08 11:21:13 -0800

    Testing form

---
+++ scripts/controllers/routes.js
@@ -0,0 +1,7 @@
+'use strict';
+
+page('/', app.bookView.initIndexPage);
+page('/detail', app.detailView.initIndexPage);
+page('/new', app.addView.initIndexPage);
+
+page();
---
+++ scripts/views/add-book.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+  const addView = {};
+
+  addView.submit = event => {
+    event.preventDefault();
+    let book = new app.Book({
+      author: $('#author').val(),
+      title: $('#title').val(),
+      isbn: $('#isbn').val(),
+      image_url: $('#image_url').val(),
+      description: $('#description').val(),
+    })
+    book.insertBook();
+    window.location = '../';
+  }
+
+  addView.initIndexPage = function() {
+    $('.container').hide();
+    $('.book-form').show();
+  }
+
+  module.addView = addView;
+
+})(app);
